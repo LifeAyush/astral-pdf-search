68ffa6951d9dc3cee06a18f65458cd8a5985b472
Ayush Patil <97448401+LifeAyush@users.noreply.github.com>
2025-05-02 20:13:04 +0530

    [misc] Added domains for pdf image retrieval

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'encrypted-tbn0.gstatic.com',
+      'tbn0.gstatic.com',
+      'tbn1.gstatic.com',
+      'tbn2.gstatic.com',
+      'tbn3.gstatic.com',
+      'localhost',
+      '127.0.0.1'
+    ],
+  },
+}
+
+module.exports = nextConfig 
